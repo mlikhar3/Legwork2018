9e9a774bccd10398ad9a5f60bef9cc424bd8dd7d
Drew Dahlman <info@drewdahlman.com>
2017-10-27 09:10:59 -0600

    reverting

--- src/components/content-view/ProjectView.vue
+++ src/components/content-view/ProjectView.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class='content-cell project' v-bind:style="{ 'background-image': 'url(' + project.hero_image.sizes.medium_large + ')' }">
+	<div class='content-cell project'>
 		{{project.project_name}}
 	</div>
 </template>
